e6ff45a1d6d3d3be0bbf5ab718fd50cc3b6fb9e7
hDmtP <pritamdhara914@gmail.com>
2021-11-08 00:48:06 +0530

    v0.0.1@got in touch with SubredditStream, more to learn

---
+++ SubredditStream_in_PRAW/main.py
@@ -0,0 +1,36 @@
+import praw
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+def authenticate():
+    reddit = praw.Reddit(
+        # username=os.environ.get("username"),
+        # password=os.environ.get("password"),
+        client_id=os.environ.get("id"),
+        client_secret=os.environ.get("secret"),
+        user_agent='random bullshit go brrr..',
+    )
+    return reddit
+
+reddit = authenticate()
+
+print(authenticate().read_only)
+'''
+for comment in authenticate().subreddit("iama").stream.comments():
+    # print(comment)
+    pass
+
+subreddit = reddit.subreddit("iama")
+for comment in subreddit.stream.comments(skip_existing=True):
+    # print(comment)
+    pass
+'''
+
+for submission in reddit.subreddit("memes").stream.submissions():
+    # print(submission)
+    print("\n"+submission.title)
+
